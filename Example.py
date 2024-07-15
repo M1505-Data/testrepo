14ee3819678c8bb5422a56c3496836dc5cdfd673
M1505-Data <muskaankhatri1505@gmail.com>
2024-07-15 16:31:50 +0530

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,33 @@
+for i in range(1,5): 
+
+      if (i!=1): 
+
+          print(i)
+
+
+
+
+for i in range(1,5): 
+
+      if (i!=2):  
+
+          print(i)
+
+
+
+
+for i in range(0,5): 
+
+      if (i!=1): 
+
+          print(i)
+
+
+
+
+for i in range(1,5): 
+
+       if (i==2): 
+
+           print(i)
+
